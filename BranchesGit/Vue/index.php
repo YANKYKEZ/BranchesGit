cec5061394998174117547549b7d9e5ed6622894
YANKYKEZ <yarnoghislain@yahoo.fr>
2017-11-17 15:20:08 +0100

    Add files via upload

---
+++ BranchesGit/Vue/index.php
@@ -0,0 +1,24 @@
+<!Doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8"/>
+    <title>Etude git</title>
+</head>
+
+<body>
+<div>
+    <h2>Etude Global de Git</h2>
+</div>
+    <div>
+        <h3>LES Branches</h3>
+        <p>
+            La commande gitk permet de voir les modiffications qui ont ete apporte
+            au cours du projet.
+        </p>
+
+        <h3>Mon premier push</h3>
+    </div>
+
+
+</body>
+</html>
---
+++ BranchesGit/Vue/indexArno.php
@@ -0,0 +1,1 @@
+ceci est simplement pour les commentaires.
